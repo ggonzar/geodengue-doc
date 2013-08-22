826d8bf9992404bc37836ef2214e76876218bf04
Maximiliano Báez González <mxbg.py@gmail.com>
2013-08-22 01:04:06 -0400

    se actualizan las reglas del clean

--- generate.sh
+++ generate.sh
@@ -4,8 +4,19 @@ NAME=tesis
 
 #~ Function que sirve para eliminar los archivos temporales
 clean(){
-    rm -r *.aux
-    rm *.log *.bbl *.blg *.bcf *.run.xml *.lof *.lot *.toc *.sublime-project *.sublime-workspace *.fdb_latexmk *.orig *.loa
+    rm -r *.synctex.gz
+    find . -type f -name "*.aux" -exec rm -f {} \;
+    find . -type f -name "*.log" -exec rm -f {} \;
+    find . -type f -name "*.bbl" -exec rm -f {} \;
+    find . -type f -name "*.blg" -exec rm -f {} \;
+    find . -type f -name "*.bcf" -exec rm -f {} \;
+    find . -type f -name "*.run.xml" -exec rm -f {} \;
+    find . -type f -name "*.lof" -exec rm -f {} \;
+    find . -type f -name "*.toc" -exec rm -f {} \;
+    find . -type f -name "*.fdb_latexmk" -exec rm -f {} \;
+    find . -type f -name "*.orig" -exec rm -f {} \;
+    find . -type f -name "*.loa" -exec rm -f {} \;
+    #~ rm *.log *.bbl *.blg *.bcf *.run.xml *.lof *.lot *.toc e *.fdb_latexmk *.orig *.loa
 }
 #~ Funcion que costruye
 build(){
